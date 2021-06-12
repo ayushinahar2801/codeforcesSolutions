78fb1afad5b56f193a6155b848f7ee79575ac7b0
Ayushi Nahar <85583621+ayushinahar2801@users.noreply.github.com>
2021-06-12 22:20:21 +0530

    Update CollectingBeatsInFun.cpp

--- CollectingBeatsInFun.cpp
+++ CollectingBeatsInFun.cpp
@@ -6,21 +6,28 @@ int main()
     int k;
     cin>>k;
     string arr[4];
+    
+    //Taking in array
     for(int i=0;i<4;++i)
     {
         cin>>arr[i];
     }
+    
+    //converting it into Single String
     string fullString=arr[0]+arr[1]+arr[2]+arr[3];
-    // cout<<fullString;
+    
+    //Storing all the integer in the String in a vector expelling all '.'s
     vector<int> v;
     for(int i=0;i<16;++i)
     {
         if(fullString[i]!='.')
         {
-            v.push_back(int(fullString[i])-48);
+            v.push_back(int(fullString[i])-48);//converting char to int--------->  int(char)-48
         }
     }
     int flag=0;
+    
+    //Final O/p
     for(int i=0;i<9;++i)
     {
         int cnt=count(v.begin(),v.end(),i+1);
